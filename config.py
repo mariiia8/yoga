b4b4545e4acb3ab0dc65a4bfd48f1bfb91f4e083
mariiia8 <146435960+mariiia8@users.noreply.github.com>
2026-01-28 03:14:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "7568559043:AAGQZ1dcdyC6hM0qDAQkKPyBiVo5bSQVbII"  # Получить у @BotFather
+BOT_TOKEN = ""  # Получить у @BotFather
 WEBAPP_URL = "https://ec16a70313881285ad402055b58b53d4.serveo.net"  # Или локальный адрес при тесте
 DATABASE_URL = "sqlite:///yoga_studio.db"  # Путь к SQLite
